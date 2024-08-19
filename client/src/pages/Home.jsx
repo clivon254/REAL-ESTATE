4b120158e2d69b986806531137d7e2eba81ce882
Clivon Osire <clivon254@icloud.com>
2024-08-19 15:57:57 +0300

    correcting

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -204,7 +204,7 @@ export default function Home() {
 
             <div className="my-3">
 
-              <h2 className="text-2xl font-semibold text-slate-600">Recent places for sale</h2>
+              <h2 className="text-2xl font-semibold text-slate-600">Recent place for sale</h2>
 
               <Link className="text-sm text-blue-800 hover:underline" to={'/search?offer=true'}>
                 show more offers
